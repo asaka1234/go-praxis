47c90cc187304db54b4a75e23d0fd4fb77ef7379
cyrildou <cyrildou@live.cn>
2025-05-17 01:14:56 +0800

    up

--- entity.go
+++ entity.go
@@ -178,7 +178,7 @@ type PraxisBackReqCardData struct {
 
 // =============
 // 返回给三方的
-type PraxisDepositBackRsp struct {
+type PraxisBackRsp struct {
 	Status      *int   `json:"status"` // Using pointer to allow null/nil
 	Description string `json:"description"`
 	Version     string `json:"version"`
